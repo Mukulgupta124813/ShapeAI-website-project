01fc16b4265835f41f3afdda69931d961a5cd1a7
Mukul Gupta <86305962+Mukulgupta124813@users.noreply.github.com>
2021-06-26 20:52:23 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React .js</h1>
+      <p>A bsic web dev React Js Bootchamp</p>
+    </div>
+  );
+}
+
+export default Info;
